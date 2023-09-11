ccc145803cb4b16c50023aba91fe1e7a666adf33
sleep_drifter <mattwujek@gmail.com>
2023-09-11 14:32:59 -0700

    js cleanup

--- js/base.js
+++ js/base.js
@@ -18,30 +18,30 @@ var createIGPost = function (igObj) {
   //$('#theDiv').prepend('<img src="'+thumbnail+'" />')
   
 }
-var runMedia = function (mediaIDs) {
-  console.log(mediaIDs)
-  var mediaArray = mediaIDs.media.data;
+// var runMedia = function (mediaIDs) {
+//   console.log(mediaIDs)
+//   var mediaArray = mediaIDs.media.data;
 
-  for (var i = mediaArray.length - 1; i >= 0; i--) {
+//   for (var i = mediaArray.length - 1; i >= 0; i--) {
     
-    if(i < 3 ){
-      var igURL = 'https://graph.instagram.com/' + mediaArray[i].id + '?fields=media_type,media_url,permalink,thumbnail_url,timestamp&access_token='+token;
-      fetch(igURL)
-        .then(response => response.json())
-        .then(json => createIGPost(json))
+//     if(i < 3 ){
+//       var igURL = 'https://graph.instagram.com/' + mediaArray[i].id + '?fields=media_type,media_url,permalink,thumbnail_url,timestamp&access_token='+token;
+//       fetch(igURL)
+//         .then(response => response.json())
+//         .then(json => createIGPost(json))
 
-      //console.log(mediaCount)    
+//       //console.log(mediaCount)    
       
-      //medias.push(mediaArray[i])
-      //createIGPost(mediaArray[i].id)
-    }
-  }
-  //console.log(medias)
-}
+//       //medias.push(mediaArray[i])
+//       //createIGPost(mediaArray[i].id)
+//     }
+//   }
+//   //console.log(medias)
+// }
 //fetch code
-var fetcher = fetch(igBase)
-   .then(response => response.json())
-   .then(json => runMedia(json))
+// var fetcher = fetch(igBase)
+//    .then(response => response.json())
+//    .then(json => runMedia(json))
   
 // output: {"success":"true"}
 
@@ -58,59 +58,59 @@ jQuery( document ).ready( function() {
 
 
 
-var flkty;
-var textInfo = $('#slido');
-  $('.flickity-slideshow').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true,
-    prevNextButtons: false,
-    adaptiveHeight: true,
-    imagesLoaded: true,
-    // autoPlay: 4000,
-    wrapAround: true,
-    on: {
-    change: function( index ) {
-        //console.log(flkty.selectedElements);
-        var index = flkty.selectedIndex
-        console.log(index)
-        var active = $('.flickity-slideshow .slide-item').eq(index);
-        var attr = active.attr('info-text')
-        textInfo.text(attr)
-      }
-    }
+// var flkty;
+// var textInfo = $('#slido');
+//   $('.flickity-slideshow').flickity({
+//     // options
+//     cellAlign: 'left',
+//     contain: true,
+//     prevNextButtons: false,
+//     adaptiveHeight: true,
+//     imagesLoaded: true,
+//     // autoPlay: 4000,
+//     wrapAround: true,
+//     on: {
+//     change: function( index ) {
+//         //console.log(flkty.selectedElements);
+//         var index = flkty.selectedIndex
+//         console.log(index)
+//         var active = $('.flickity-slideshow .slide-item').eq(index);
+//         var attr = active.attr('info-text')
+//         textInfo.text(attr)
+//       }
+//     }
 
-  });
+//   });
 
-flkty = $('.flickity-slideshow').data('flickity')
+// flkty = $('.flickity-slideshow').data('flickity')
 
 // lottie
-var animation1 = bodymovin.loadAnimation({
-  container: document.getElementById('lottie-1'), // Required
-  path: 'lottie/melol.json', // Required
-  renderer: 'svg', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-  //name: "Hello World", // Name for future reference. Optional.
-})
+// var animation1 = bodymovin.loadAnimation({
+//   container: document.getElementById('lottie-1'), // Required
+//   path: 'lottie/melol.json', // Required
+//   renderer: 'svg', // Required
+//   loop: true, // Optional
+//   autoplay: true, // Optional
+//   //name: "Hello World", // Name for future reference. Optional.
+// })
 
-var animation2 = bodymovin.loadAnimation({
-  container: document.getElementById('lottie-2'), // Required
-  path: 'lottie/bolt.json', // Required
-  renderer: 'svg', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-  //name: "Hello World", // Name for future reference. Optional.
-})
+// var animation2 = bodymovin.loadAnimation({
+//   container: document.getElementById('lottie-2'), // Required
+//   path: 'lottie/bolt.json', // Required
+//   renderer: 'svg', // Required
+//   loop: true, // Optional
+//   autoplay: true, // Optional
+//   //name: "Hello World", // Name for future reference. Optional.
+// })
 
-var animation3 = bodymovin.loadAnimation({
-  container: document.getElementById('lottie-3'), // Required
-  path: 'lottie/van.json', // Required
-  renderer: 'svg', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-  //name: "Hello World", // Name for future reference. Optional.
-})
+// var animation3 = bodymovin.loadAnimation({
+//   container: document.getElementById('lottie-3'), // Required
+//   path: 'lottie/van.json', // Required
+//   renderer: 'svg', // Required
+//   loop: true, // Optional
+//   autoplay: true, // Optional
+//   //name: "Hello World", // Name for future reference. Optional.
+// })
 
 var animation3 = bodymovin.loadAnimation({
   container: document.getElementById('lottie-4'), // Required
@@ -121,8 +121,8 @@ var animation3 = bodymovin.loadAnimation({
   //name: "Hello World", // Name for future reference. Optional.
 })
 
-animation1.setSpeed(1.25)
-animation2.setSpeed(1.25)
+// animation1.setSpeed(1.25)
+// animation2.setSpeed(1.25)
 animation3.setSpeed(1.25)
 
 
